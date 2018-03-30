8572b49e737d36688ffdfd68153d43e1be662121
Vitaliy <vitaliy.saviour@gmail.com>
2018-03-30 22:27:07 +0300

    Правки для перепроверки

--- js/modal-form.js
+++ js/modal-form.js
@@ -5,8 +5,8 @@ var checkIn = modal.querySelector("[name=check-in]");
 var checkOut = modal.querySelector("[name=check-out]");
 var adult = modal.querySelector("[name=adult]");
 var children = modal.querySelector("[name=children]");
-var storageAdult = localStorage.getItem("adult");
-var storageChildren = localStorage.getItem("children");
+var storageAdult = "";
+var storageChildren = "";
 var isStorageSupport = true;
 
 try {
@@ -21,23 +21,26 @@ modal.classList.add("modal-form-hide");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.toggle("modal-form-show");
+  modal.classList.remove("modal-form-error");
   checkIn.focus();
   if (storageAdult) {
-  	adult.value = storageAdult;
+    adult.value = storageAdult;
   }
   if (storageChildren) {
-  	children.value = storageChildren;
+    children.value = storageChildren;
   }
 });
 
 form.addEventListener("submit", function (evt) {
   if (!checkIn.value || !checkOut.value || !adult.value || !children.value) {
-  	evt.preventDefault();
-  	console.log("Необходимо ввести все данные");
+    evt.preventDefault();
+    modal.classList.remove("modal-form-error");
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add("modal-form-error");
   } else {
-  	if (isStorageSupport) {
-  	  localStorage.setItem("adult", adult.value);
-  	  localStorage.setItem("children", children.value);
-  	}
+    if (isStorageSupport) {
+      localStorage.setItem("adult", adult.value);
+      localStorage.setItem("children", children.value);
+    }
   }
 });
